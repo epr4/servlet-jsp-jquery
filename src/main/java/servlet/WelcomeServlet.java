bbb443d7572aa33734d729b3bc5bf4ad478a3695
Eko P <abumkha@gmail.com>
2023-05-06 17:57:40 +0700

    WelcomeServlet

---
+++ src/main/java/servlet/WelcomeServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        name = "WelcomeServlet",
+        urlPatterns = {"/welcome"}
+)
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String message = request.getParameter("message");
+        request.setAttribute("text", message);
+        request.getRequestDispatcher("/ExampleThree.jsp").forward(request, response);
+    }
+
+/*
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("Welcome".getBytes());
+        out.flush();
+        out.close();
+    }
+*/
+
+}
